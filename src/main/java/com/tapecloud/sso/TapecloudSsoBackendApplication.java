3eb7f2645add3eab9738b5b559eeae5521d20334
ThXPs <b.cruceno@galileo.edu.ar>
2026-09-01 19:01:04 -0300

    feat: agregar plantilla del proyecto Spring Boot

---
+++ src/main/java/com/tapecloud/sso/TapecloudSsoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tapecloud.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapecloudSsoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapecloudSsoBackendApplication.class, args);
+	}
+
+}
