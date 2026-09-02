45ea1ec55184d06f98d08d898c85c8906f72f756
bcruce <b.cruce.th@gmail.com>
2026-09-02 12:28:55 -0300

    feat: integrar frontend y corregir autenticacion

--- src/main/java/com/tapecloud/sso/user/repository/AppUserRepository.java
+++ src/main/java/com/tapecloud/sso/user/repository/AppUserRepository.java
@@ -2,12 +2,14 @@ package com.tapecloud.sso.user.repository;
 
 import com.tapecloud.sso.user.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface AppUserRepository extends JpaRepository<AppUser, UUID> {
 
+    @EntityGraph(attributePaths = "roles")
     Optional<AppUser> findByEmailIgnoreCase(String email);
 
     boolean existsByEmailIgnoreCase(String email);
