63c75d518be24052c2091de96429c0a9a7acc3ee
Leandro Sanchez <lsanchez@viacorreo.com.ar>
2026-09-04 11:10:05 -0300

    feat: initialize tapecloud auth core

---
+++ src/main/java/com/tapecloud/auth/TapecloudAuthCoreApplication.java
@@ -0,0 +1,11 @@
+package com.tapecloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapecloudAuthCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TapecloudAuthCoreApplication.class, args);
+    }
+}
